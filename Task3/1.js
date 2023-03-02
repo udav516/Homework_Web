afed32e4eb7e0ba358b5b3a70a7a9737f2fa71fd
udav516 <eldar.udav516@yandex.ru>
2023-03-02 17:49:23 +0300

    change celsius, fahrenheit_elevation, tofixed()

--- Task3/1.js
+++ Task3/1.js
@@ -1,6 +1,6 @@
-let temp = Number.parseFloat(prompt('Введите температуру в градусах Цельсия: '));
-alert(`Цельсий: ${temp}, Фаренгейт: ${transformations(temp)}`);
+const celsius = Number.parseFloat(prompt('Введите температуру в градусах Цельсия: '));
+alert(`Цельсий: ${celsius}, Фаренгейт: ${fahrenheit_elevation(celsius.toFixed(1))}`);
 
-function transformations(temp) {
-    return ((9 / 5) * temp + 32).toFixed(1);
+function fahrenheit_elevation(celsius) {
+    return (9 / 5) * celsius + 32;
 }
